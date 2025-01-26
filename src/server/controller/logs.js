578b056639686d0d4f3e5cf092768631eaeb6164
varadpundlik <varadpundlik@gmail.com>
2025-01-26 15:07:08 +0530

    fix: fetch all logs

--- src/server/controller/logs.js
+++ src/server/controller/logs.js
@@ -7,18 +7,32 @@ const client = new Client({
 const fetchLogs = async (req,res) => {
     const { index } = req.params;
     try {
-        const response = await client.search({
-            index,
-            body: {
-                query: {
-                    match_all: {},
+        const scrollSize = 1000; // Number of documents to fetch per scroll
+            let documents = [];
+            let response = await client.search({
+                index,
+                scroll: '1m', // Set the scroll timeout
+                size: scrollSize,
+                body: {
+                    query: {
+                        match_all: {},
+                    },
                 },
-            },
-        });
+            });
 
-        const documents = response.hits.hits.map(hit =>hit._source.message);
-        console.log('Fetched logs numbers:', documents.length);
-        res.json(documents);
+            while (response.hits.hits.length > 0) {
+                documents = documents.concat(response.hits.hits.map(hit => hit._source.message));
+                const { _scroll_id } = response;
+
+                // Fetch the next batch of results
+                response = await client.scroll({
+                    scroll_id: _scroll_id,
+                    scroll: '1m',
+                });
+            }
+
+            console.log(`Fetched total logs: ${documents.length}`);
+            res.json(documents);
     } catch (error) {
         console.error('Error fetching logs:', error);
         res.status(500).json({ error: 'An error occurred while fetching logs.' });
