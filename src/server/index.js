5751e20ac2baa7557ad84a76bf101f5c867e00a2
divyank007 <divyanksagvekar37@gmail.com>
2025-03-17 12:57:02 +0530

    Save local changes before merging main

--- src/server/index.js
+++ src/server/index.js
@@ -4,9 +4,11 @@ const { logsRouter } = require("./routes/logRoutes");
 const { metricsRouter } = require("./routes/metrics");
 const { rcaRouter } = require("./routes/rca");
 const { automationRouter } = require("./routes/automation");
-
+const cors = require("cors");
 const app = express();
 const port = 5000;
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/logs", logsRouter);
