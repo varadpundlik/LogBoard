c6a0ee1f8596dfbc68053c53e3052e431ef2f3c4
Anirudha1821 <udgirkaranirudha@gmail.com>
2025-03-19 22:12:39 +0530

    removed indexData

--- src/server/routes/metrics.js
+++ src/server/routes/metrics.js
@@ -1,9 +1,7 @@
 const metricsRouter = require("express").Router();
 
 const { fetchMetrics } = require("../controller/metrics");
-const { addIndexData } = require("../controller/indexData");
 
 metricsRouter.get("/:index", fetchMetrics);
-metricsRouter.post("/addIndex", addIndexData);
 
 module.exports = { metricsRouter };
